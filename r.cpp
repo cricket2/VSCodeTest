8ca8fc254e337bbf4897aafa4aa3a6c9d4847bdf
cricket2 <kenobiobi@onet.pl>
2017-06-21 07:23:31 +0200

    commit 3

--- r.cpp
+++ r.cpp
@@ -2,8 +2,19 @@
 
 using namespace::std;
 
+class newClass{
+    public:
+    int i;
+    newClass(int t=0) i(t) :{
+
+        
+    };
+};
+
 int main(){
+
     cout<<"Nie no, poprawię";
     cout<<"\nHello in C++";
-
+    cout<<"Hello world in C++";
+    int w;
 }
--- t.c
+++ t.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
+    int fy;
     printf("Hello world in C");
     return 0;
 }
